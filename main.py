ba4ea84bdd092a7685991b021922571cd6b70843
gorgg7 <georgeragaee6@gmail.com>
2025-10-02 10:02:29 +0300

    subtract

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 x=input("Enter a number:")
 y=input("Enter another number:")
 z=int(x)+int(y)
+sub=int(x)-int(y)
 print("The sum of {0} and {1} is {2}".format(x,y,z))
+print("The subtraction of {0} and {1} is {2}".format(x,y,sub))
